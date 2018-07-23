63ed392dbcaff8cc5b5c10822efa3b07bebc302d
Samir Mayfield <samirmayfield@live.com>
2018-07-23 16:04:23 -0700

    C# Drill 1
    printing text to console that has quotes inside of string.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+
+    class Program
+    {
+        static void Main()
+        {
+
+            
+         Console.WriteLine("Welcome to Acme Accounting Systems");
+         Console.WriteLine("Remember, we're \"Accounting\" on you!");
+         Console.Read();
+
+        }
+    }
+
