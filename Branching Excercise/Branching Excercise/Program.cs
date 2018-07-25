7b405c22cab7f2456b592623ce091ab88fcdc73e
Samir Mayfield <samirmayfield@live.com>
2018-07-25 16:00:21 -0700

    added developer notes to explain process

--- Branching Excercise/Branching Excercise/Program.cs
+++ Branching Excercise/Branching Excercise/Program.cs
@@ -6,12 +6,15 @@ namespace Branching_Excercise
     {
         static void Main()
         {
+            //Strings answer then converts to double//
             Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
             Console.WriteLine("Please enter a package weight");
             string packageWeight = Console.ReadLine();
             double myWeight = Int32.Parse(packageWeight);
+            //If weight is less then 50 proceed//
             if (myWeight < 50)
             {
+                //Strings answer then converts to double//
                 Console.WriteLine("Please enter the package width");
                 string packageWidth = Console.ReadLine();
                 double myWidth = Int32.Parse(packageWidth);
@@ -22,6 +25,7 @@ namespace Branching_Excercise
                 string packageLength = Console.ReadLine();
                 double myLength = Int32.Parse(packageLength);
                 double totalDimensions = myLength + myHeight + myWidth;
+                //If dimensions total less than 50 proceed//
                 if (totalDimensions < 50)
                 {
                     double priceResult = totalDimensions * myWeight / 100;
@@ -30,6 +34,7 @@ namespace Branching_Excercise
                     Console.ReadLine();
 
                 }
+                //If dimensions total 50 or more then do not proceed//
                 else
                 {
                     Console.WriteLine("Package too big to be shipped via Package Express.");
@@ -38,6 +43,7 @@ namespace Branching_Excercise
 
                 
             }
+            //If weight is 50 or greater then do not proveed//
             else
             {
                 Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
