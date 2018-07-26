1c632079781bda0b0be9382417b153f8cde2ddba
Samir Mayfield <samirmayfield@live.com>
2018-07-25 19:14:08 -0700

    added demonstration of concatenating, converting, and using stringbuilder

---
+++ ConcatenatingConvertingStringbuilder/ConcatenatingConvertingStringbuilder/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace ConcatenatingConvertingStringbuilder
+{
+    class Program
+    {
+        static void Main()
+        {
+            string stringOne = "Hello, my name is ";
+            string stringTwo = "Samir, ";
+            string stringThree = "it is nice to make your acquaintance!";
+            string allStrings = stringOne + stringTwo + stringThree;
+            string upperCase = allStrings.ToUpper();
+            Console.WriteLine(upperCase);
+            //Use AppendLine//
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine("One");
+            builder.AppendLine();
+            builder.AppendLine("Two").AppendLine("Three");
+
+            //Display//
+            Console.Write(builder);
+
+            //AppendLine uses \r\n sequences//
+            Console.WriteLine(builder.ToString().Contains("\r\n"));
+            Console.ReadLine();
+        }
+       
+    }
+
+    
+}
