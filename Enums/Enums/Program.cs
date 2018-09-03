3e8c0d4cab8f1f83d85f1662429ee9d48b71d0e7
Samir Mayfield <SamirMayfield@live.com>
2018-09-03 11:52:48 -0700

    Add enum exercise

---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,36 @@
+using System;
+
+class Program
+{
+    
+    static void Main()
+    {
+        try
+        {
+            
+            Console.WriteLine("Please enter a day of the week");
+            string today = Console.ReadLine(); 
+
+            DayOfWeek day = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), today.ToUpper()); //Converts string to enum data type, also converts string variable to uppercase so it can be matched properly           
+            Console.ReadLine();
+           
+        }
+        catch
+        {
+            Console.WriteLine("Please enter an actual day of the week please");
+            Console.ReadLine();
+        }
+    }
+    public enum DayOfWeek
+    {
+        MONDAY,
+        TUESDAY,
+        WEDNESDAY,
+        THURSDAY,
+        FRIDAY,
+        SATURDAY,
+        SUNDAY
+    }
+
+
+}
