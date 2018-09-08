06d38ee12aaa4e5fe8d880ceb15a684614830531
Samir Mayfield <SamirMayfield@live.com>
2018-09-07 17:34:05 -0700

    add console app that prints current date and time to console

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var time = DateTime.Now;
+            Convert.ToString(time);
+            Console.WriteLine(time);
+            Console.ReadLine();
+        }
+    }
+
