2379e529c4e604acd238397dc434f99a75ed28c1
Samir Mayfield <SamirMayfield@live.com>
2018-09-08 02:37:27 -0700

    add datetime exercise

---
+++ DatTime Exercise/DatTime Exercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var time = DateTime.Now;       
+        Console.WriteLine("Please enter a number");
+        int answer = Convert.ToInt32(Console.ReadLine());        
+        DateTime currentTime = DateTime.Now;
+        DateTime twoHoursLater = currentTime.AddHours(answer);
+        Console.WriteLine(string.Format("{0} {1}", currentTime, twoHoursLater));
+        Console.ReadLine();
+    }
+}
