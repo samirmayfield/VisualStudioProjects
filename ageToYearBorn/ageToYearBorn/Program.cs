c57bb51b2327f11ff3df4ddd3a8d1d652376f710
Samir Mayfield <SamirMayfield@live.com>
2018-09-08 03:43:55 -0700

    add years born exercise

---
+++ ageToYearBorn/ageToYearBorn/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ageToYearBorn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How old are you? ");
+            try
+            {
+                int age = (Convert.ToInt32(Console.ReadLine()));
+                if (age < 1) throw new ArgumentOutOfRangeException();
+                DateTime bday = DateTime.Now.AddYears(-age);
+                Console.WriteLine("You were born in " + bday.Year);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Your number must be greater than 0");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter a whole number.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unknown error");
+            }
+            Console.ReadLine();
+        }
+    }
+}
