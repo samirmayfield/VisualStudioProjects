15a557552c2d8c093b61d37e757e2c89c0c479f2
Samir Mayfield <SamirMayfield@live.com>
2018-09-08 03:15:39 -0700

    Ass chanining constructors exercise

---
+++ ChainingConstructor/ChainingConstructor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace CSharpDrill22
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+            const string messageTwoPart1 = "Hello there, my name";
+            const string messageTwoPart2 = " is Samir Mayfield";
+            
+            var messageDisplayTwo = new MessageBuilder(messageTwoPart1, messageTwoPart2);
+           
+            messageDisplayTwo.DisplayMessage();
+            Console.ReadLine();
+        }
+    }
+    class MessageBuilder
+    {
+        public MessageBuilder(string oneMessage) : this(oneMessage, "") { }
+        public MessageBuilder(string oneMessage, string secondMessage)
+        {
+            FullMessage = oneMessage + secondMessage;
+        }
+        public string FullMessage { get; set; }
+        public void DisplayMessage()
+        {
+            Console.WriteLine(FullMessage);
+        }
+    }
+}
